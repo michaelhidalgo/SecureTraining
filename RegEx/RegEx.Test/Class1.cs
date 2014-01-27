7a57d03ae1350ef1baa51fe624c073f913ade981
Michael Hidalgo <michaelfallas@gmail.com>
2014-01-27 15:05:32 -0600

    Adding a unit test project

---
+++ RegEx/RegEx.Test/Class1.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+namespace RegEx.Test
+{
+   
+    public class RegularExpressionsTest
+    {
+        private const string NumberPattern = "^d/$";
+        private static readonly string EmailPattern=@"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$";
+        private static readonly string ZipCodePatten = "";
+
+        [Test]
+        public void IsNumeric()
+        {
+            var input = "123455";
+            var result = RegularExpressions.Match(input, NumberPattern);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void IsNotNumeric ()
+        {
+            var input = "01234565##";
+            var result = RegularExpressions.Match(input, NumberPattern);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsEmail()
+        {
+            var input = "michael.hidalgo@owasp.org";
+            var result = RegularExpressions.Match(input, EmailPattern);
+            Assert.IsTrue(result);
+        }
+
+
+        [Test]
+        public void NotValidEmail()
+        {
+            var watcher = new Stopwatch();
+            var input = "michaelhidalgoowaspsmichaelhidalgoowasps.org";
+            watcher.Start();
+            var result = RegularExpressions.Match(input, EmailPattern);
+            watcher.Stop();
+            Debug.WriteLine(string.Format("Tiempo transcurrido {0} segundos", watcher.ElapsedMilliseconds/1000.0m));
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ RegEx/RegEx/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegEx
+{
+    public class RegularExpressions
+    {
+        public static bool  Match (string input, string patern)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(input, patern);
+        }
+    }
+}
